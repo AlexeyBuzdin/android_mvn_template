627feceea14b68b334437e62619c6babb1b53bfb
LArchaon <alex.buzdin@gmail.com>
2013-03-20 02:22:12 +0200

    Added RoboGuice and ActionBarSherlock

--- src/lv/buzdin/alex/example/MyActivity.java
+++ src/lv/buzdin/alex/example/MyActivity.java
@@ -2,8 +2,10 @@ package lv.buzdin.alex.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
+import roboguice.activity.RoboActivity;
 
-public class MyActivity extends Activity {
+public class MyActivity extends RoboSherlockActivity {
     /**
      * Called when the activity is first created.
      */
